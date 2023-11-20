1cca0e1e50b99b94d0c3be55cc19eb897f0212e0
cogsincogs <jamiewthomas13@gmail.com>
2023-11-20 19:36:37 +0000

    Add helper function to read shader files

---
+++ shaderHelper.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+char* parseShader(const char *path) {
+    /* Shader file reading */
+    FILE* shaderFile = fopen(path, "r");
+    int fileSize = 0;
+    char* shaderSource = NULL;
+
+    // Getting file size
+    fseek(shaderFile, 0, SEEK_END);
+    fileSize = ftell(shaderFile);
+    rewind(shaderFile);
+
+    // Reading from file
+    shaderSource = (char*)malloc(sizeof(char) * (fileSize + 1));
+    fread(shaderSource, sizeof(char), fileSize, shaderFile);
+    shaderSource[fileSize] = '\0';
+    fclose(shaderFile);
+
+    return shaderSource;
+}
