bfd5098322630ca392956eb6331319c7828b6115
cogsincogs <jamiewthomas13@gmail.com>
2023-12-04 02:12:34 +0000

    Cleanup & add comments to VertexBufferLayout class

--- graphics/opengl/vertexBufferLayout.cpp
+++ graphics/opengl/vertexBufferLayout.cpp
@@ -1,14 +1,13 @@
 #include "vertexBufferLayout.h"
 
-VertexBufferLayout::VertexBufferLayout()
-    : m_Stride(0)
-{
-}
+#include <glad/glad.h>
 
-VertexBufferLayout::~VertexBufferLayout()
-{
-}
 
+/**
+ * @brief Make space in the vertex buffer for some data
+ * @param type (GLenum) The type of the incoming data
+ * @param count How many of the aforementioned data type will be stored
+*/
 void VertexBufferLayout::push(const unsigned int type, const unsigned int count)
 {
     m_Elements.push_back({type, count, GL_FALSE});
--- graphics/opengl/vertexBufferLayout.h
+++ graphics/opengl/vertexBufferLayout.h
@@ -1,13 +1,24 @@
 #pragma once
 
-#include <glad/glad.h>
 #include <vector>
+
+// * Including these here so I don't have to include the whole of glad.c in this header file
+#define GL_UNSIGNED_BYTE 0x1401
+#define GL_UNSIGNED_INT 0x1405
+#define GL_FLOAT 0x1406
+
 
 struct VertexBufferElement {
     const unsigned int type;
     const unsigned int count;
     const unsigned int normalized;
 
+    /**
+     * @brief Get type size in bytes from type GLenum
+     * @param type (GLenum) The type enum
+     * @return sizeof() on fundamental type
+     * @throws Returns -1 if anything other than uint, float or char requested (for now)
+    */
     static unsigned int getSizeOfType(const unsigned int type) {
         switch (type) {
             case GL_UNSIGNED_INT: return sizeof(unsigned int);
@@ -18,14 +29,15 @@ struct VertexBufferElement {
     }
 };
 
+
 class VertexBufferLayout {
 private:
     std::vector<VertexBufferElement> m_Elements;
     unsigned int m_Stride;
 
 public:
-    VertexBufferLayout();
-    ~VertexBufferLayout();
+    VertexBufferLayout() : m_Stride(0) {}
+    ~VertexBufferLayout() {}
 
     void push(const unsigned int type, const unsigned int count);
 
