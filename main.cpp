d978c3531758528195f97474a4bfd135cbb21968
cogsincogs <jamiewthomas13@gmail.com>
2023-12-06 23:39:57 +0000

    Clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -14,35 +14,11 @@ const int WIDTH = 800;
 const int HEIGHT = 600;
 
 /**
- * DONE: Improve directory structure
- * DONE: Make everthing that can be const, const
- * DONE: Rename Entity class to Model
- * DONE: Create parent class of Entity called Renderable, contains render() method
- * DONE: Add std::vector<Renderable> to Renderer called m_RenderQueue
- * DONE: Add overload of Renderer::draw() which loops through m_RenderQueue
- * DONE: Decide whether to put glDrawElements call in draw() function or Renderable::render()
- * DONE: I need to get the data to the Model class somehow before its constructor ends
- * DONE: Finish building Cube/Model/Renderable classes to be able to provide relevant info to Renderer
- * DONE: Consider moving the VAO to the Renderer class
- * DONE: Create default shaders, and add a Shader as a field of Model class
- * DONE: Create default textures, and add a Texture as a field of Model class - Should be an array of textures
- * DONE: Work out why only one cube is showing. Are they all inside each other? If so, why are the matrices not correct?
- * DONE: Implement a way to get and set rotation of Model matrix. First attempt hasn't worked, need to try again
  * TODO: Abstract matrices (may want to wait until Camera implemented) - Not going to do this until I can see a clear benefit of it
- * TODO: Make Window static so WIDTH and HEIGHT are accessible throughout program
- * DONE: Make Renderer static so I can push Renderables to the renderer in their constructors
- * DONE: Implement an OpenGL error wrapper for all GL calls
- * DONE: Add extensive comments and make code look clear and easy to read
 */
 
 int main()
 {
-
-    /**
-     * Initialise objects
-     */
-
-    //const Window *window = new Window(WIDTH, HEIGHT, "LearnOpenGL");
     Window::init(WIDTH, HEIGHT, "LearnOpenGL");
     Renderer::init();
 
@@ -79,7 +55,6 @@ int main()
 
     Renderer::cleanup();
     Window::cleanup();
-    //delete window;
 
     glfwTerminate();
     return 0;
